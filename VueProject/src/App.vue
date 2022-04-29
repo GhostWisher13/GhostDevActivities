0c1dbe77209dd70634a188869848e54ad8b97151
José Gabriel Corrêa <101403016+GhostWisher13@users.noreply.github.com>
2022-04-29 08:57:27 -0300

    Add files via upload

---
+++ VueProject/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import CabecalhoBlock from "@/components/CabecalhoBlock.vue";
+import RodapeBlock from "@/components/RodapeBlock.vue";
+import BlocoPrincipal from "@/components/BlocoPrincipal.vue";
+
+export default {
+  components: { MenuLateral, CabecalhoBlock, RodapeBlock, BlocoPrincipal, },
+};
+</script>
+<template>
+  <MenuLateral />
+  <CabecalhoBlock />
+  <RodapeBlock/>
+  <BlocoPrincipal/>
+</template>
+<style></style>
